64cec31adcb8796cf6dcd9ebf506ed92ca1c39c8
ivan <juanvillegasmorales@outlook.com>
2024-02-03 22:11:22 -0600

    Actualizar estilos globales y formulario de inicio de sesión y estilos del home

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,12 @@
 
 export default function LoginPage() {
   return (
-    <div className="sm:max-w-full  flex flex-col justify-center items-center gap-4">
+    <div className="h-screen  flex flex-row justify-center items-center gap-4">
+      <form className="flex sm: flex-col justify-center items-center gap-6 bg-slate-400 p-10 rounded-3xl ">
       <h1 className="text-3xl font-mono text-white ">Login</h1>
-      <form className="flex flex-col gap-6 ">
-        <input type="text" placeholder="Username" className="text-2xl rounded-lg p-2" />
+        <input type="text" placeholder="Username" className="text-2xl rounded-lg p-2  focus:scale-110  focus:drop-shadow-lg focus:duration-200  outline-none" />
         <input type="password" placeholder="Password" className="text-2xl rounded-lg p-2"/>
-        <button type="submit" className="bg-lime-700 p-1  rounded-lg shadow-md hover:bg-lime-600 ">Login</button>
+        <button type="submit" className="bg-lime-700 w-56  h-11   rounded-lg shadow-md hover:bg-lime-600 hover:scale-105 duration-300 border-spacing-1 drop-shadow-sm">Login</button>
       </form>
     </div>
   )
