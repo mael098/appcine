34337d90bec049685463a930e3be16137f34407a
ivan <juanvillegasmorales@outlook.com>
2024-02-05 17:57:03 -0600

    Update global and home page styles

---
+++ src/app/movis-admin/page.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+
+
+
+
+export default function movis() {
+    return(
+        
+        <div className="h-screen">
+            <main className="h-screen w-screen flex flex-grow justify-between items-center">
+                <section className="h-screen  flex flex-col basis-4/5 justify-center items-center bg-slate-600 ">
+                    
+                </section>
+            <section className="flex flex-col justify-center items-center basis-3/12 bg-orange-400 h-screen">
+
+            </section>
+            </main>
+        </div>
+    )
+}
