c431e30e259f1e78143909f9c2ad6b8418af994e
mael098 <juanvillegasmorales@outlook.com>
2024-02-09 21:28:58 -0600

    Update background and add movie images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,84 @@
 
+import Image from 'next/image'
+
 export default function Home() {
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="h-screen w-screen overflow-x-hidden">
+            <div className="flex w-screen flex-grow p4 h-12">
+                <nav className="flex w-screen flex-grow p-3 bg-lime-500 fixed">
+                    <h1 className="text-4xl">N_cine</h1>
+                </nav>
+            </div>
+            <main className="h-5/6 grid grid-cols-4 grid-rows gap-8 m-16">
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" className='' ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" ></Image>
+                    <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
+                    <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+                </div>
+
+            </main>
+
         </div>
     )
 }
