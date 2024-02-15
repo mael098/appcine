49c2e6d919185781696aeabbae9160eae65a17a8
eliyya <tecno_eli@hotmail.com>
2024-02-15 12:20:03 -0600

    fix link

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export function MovieCard() {
     return (
-        <Link href={'/functions/2932293882938'}>
+        <Link href={'/function/2932293882938'}>
             <div className="flex flex-col items-center justify-center text-center">
                 <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" />
                 <h2 className='text-black text-2xl font-extrabold text-center'>Harry Potter: La Piedra Filosofal</h2>
