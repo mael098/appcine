2939aeaab4b2284d61dcbb2323985a62d794a951
ivan <juanvillegasmorales@outlook.com>
2024-02-14 18:52:57 -0600

    Refactor grid layout and add movie title

--- src/app/sala-movis/page.tsx
+++ src/app/sala-movis/page.tsx
@@ -3,20 +3,24 @@ export default function salaMovis() {
     return(
         <div className="h-screen w-screen overflow-x-hidden">
             <Nav/>
-            <main className="h-screen grid grid-rows-1 grid-flow-col">
-                <div className='row-start-1'>
-                    <img src="https://www.tuexpertomovil.com/wp-content/uploads/2021/06/fondos-de-pantalla-para-el-movil-de-harry-potter.jpg" alt="" />
-                </div>
-                <div className='row-start-2'>
-                    <div className='p-4 bg-lime-400'>
-                        <i>B</i>
-                        <h2>2h 23m</h2>
-                        <p>directora</p>
-                        <p>j.k. Rowling</p>
-                    </div>
+            <main className="h-screen grid grid-cols-5">
+                <div className='col-span-3'>
+                    <img src="https://www.tuexpertomovil.com/wp-content/uploads/2021/06/fondos-de-pantalla-para-el-movil-de-harry-potter.jpg" alt="" className='' />
+                    <div className='col-span-2'>
+                        <div className='p-4 bg-lime-400'>
+                            <i>B</i>
+                            <h2>2h 23m</h2>
+                            <p>directora</p>
+                            <p>j.k. Rowling</p>
+                        </div>
+                        <div className=''>
+                            <h2 className=''>harry potter y la piedra </h2>
+                        </div>
 
+                    </div>
                 </div>
-                <div className='row-start-1 shrink w-full h-full flex flex-col justify-center items-center'>
+
+                <div className='col-span-2 shrink w-full h-full flex flex-col justify-center items-center'>
                     <h1 className='text-3xl'>horarios</h1>
                     <h1 className='text-xl'>SUB</h1>
                     <div className='flex flex-row'>
