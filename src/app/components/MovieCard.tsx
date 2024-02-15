936aca306bfba545dad128b9d7f00eeab87d2f3f
ivan <juanvillegasmorales@outlook.com>
2024-02-14 19:56:38 -0600

    Update MovieCard component styling and title

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 export function MovieCard() {
     return (
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center text-center">
             <Image src='/image.jpeg' width={150} height={150} alt="Picture of the author" />
-            <h2 className='text-black text-2xl font-extrabold'>Popular Movies</h2>
-            <p className='text-xl text-slate-900 font-extrabold'>la pieda Filosofal</p>
+            <h2 className='text-black text-2xl font-extrabold text-center'>Harry Potter:
+            La Piedra Filosofal</h2>
         </div>
     )
 }
