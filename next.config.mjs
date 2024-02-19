59fe11feec630f08d2f8081f981b836336318fa9
eliyya <tecno_eli@hotmail.com>
2024-02-19 10:28:03 -0600

    fix env

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,11 @@ export default withMDX({
     rewrites: async () => ([
         {
             source: '/images/movies/:path*',
-            destination: `${process.env.NEXT_SUPABASE_URL}/storage/v1/object/public/movies/images/:path*`
+            destination: `${process.env.VERCEL_URL}/storage/v1/object/public/movies/images/:path*`
         },
         {
             source: '/images/covers/:path*',
-            destination: `${process.env.NEXT_SUPABASE_URL}/storage/v1/object/public/movies/covers/:path*`
+            destination: `${process.env.VERCEL_URL}/storage/v1/object/public/movies/covers/:path*`
         }
     ]),
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
