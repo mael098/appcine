57b77d22dda0f64ce4a9c36cbcd452fd6bd595fa
eliyya <tecno_eli@hotmail.com>
2024-02-19 22:10:09 -0600

    fix call to api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,25 @@ import { MovieCard } from './MovieCard'
 import Nav from './Nav'
 import { Database } from '@/lib/supabaseTypes'
 import { NEXT_URL } from '@/lib/constants'
+import db from '@/lib/db'
+import { notFound } from 'next/navigation'
 
 export default async function Home() {
 
-    const getMovies = async () => {
-        console.log(NEXT_URL+'/api/movies')
-        const res = await fetch(NEXT_URL+'/api/movies')
-        const data = await res.json()
-        return data as Database['public']['Tables']['movies']['Row'][]
+    const moviesQuery = await db
+        .from('movies')
+        .select('*')
+
+    if (moviesQuery.error) {
+        console.error(moviesQuery.error)
+        return notFound()
     }
-    const movies = await getMovies()
 
     return (
         <div className="h-screen w-screen overflow-x-hidden">
             <Nav/>
             <main className="grid grid-cols-4 grid-rows gap-8">
-                {movies.map(({name, id, image})=> (
+                {moviesQuery.data.map(({name, id, image})=> (
                     <MovieCard id={id} name={name} image={image} key={id}/>
                 ))}
             </main>
