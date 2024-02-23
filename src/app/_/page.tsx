3c68d035f8dd54401e6bab56ca02fc01d2942517
ivan <juanvillegasmorales@outlook.com>
2024-02-23 09:28:34 -0600

    Refactor login page layout and add image

--- src/app/_/page.tsx
+++ src/app/_/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image'
+
 export default async function AdminDashboard() {
     return (
-        <div>
-            <h1>Admin Dashboard</h1>
+        <div className="w-screen h-screen grid justify-center items-center">
+            <main className="grid grid-cols-2 justify-center items-center bg-green-200 h-96 w-[700px] rounded-2xl shadow-green-900 shadow-2xl">
+                <Image src='/Premium Photo _ Vintage cinema videocamera.jpg' alt='fondo ' width={380} height={100} className='col-span-1 w-full h-full rounded-l-2xl'/>
+                <div className='col-span-1 flex flex-col gap-9 justify-center items-center'>
+                    <h1 className="text-3xl font-extrabold">login</h1>
+                    <input type="email" name="" id="" placeholder='email' className="p-2 rounded-md shadow-gray-950 shadow-inner focus:outline-none focus:outline-emerald-700 focus:shadow-none"/>
+                    <input type="password" name="" id=""  placeholder='password' className="p-2 rounded-md shadow-gray-950 shadow-inner focus:outline-none focus:outline-emerald-700 focus:shadow-none"/>
+                </div>
+            </main>
         </div>
     )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,16 @@
+import Image from 'next/image'
 
 export default function LoginPage() {
     return (
-        <div className="h-screen flex flex-row justify-center items-center gap-4">
-            <form className="flex sm: flex-col justify-center items-center gap-6 bg-slate-400 p-10 rounded-3xl">
-                <h1 className="text-3xl font-mono text-white">Login</h1>
-                <input type="text" placeholder="Username" className="text-2xl rounded-lg p-2 focus:scale-110 focus:drop-shadow-lg focus:duration-200 outline-none" />
-                <input type="password" placeholder="Password" className="text-2xl rounded-lg p-2 focus:scale-110 focus:drop-shadow-lg focus:duration-200 outline-none"/>
-                <button type="submit" className="bg-lime-700 w-56 h-11 rounded-lg shadow-md hover:bg-lime-600 hover:scale-105 duration-300 border-spacing-1 drop-shadow-sm">Login</button>
-            </form>
+        <div className="w-screen h-screen grid justify-center items-center">
+            <main className="grid grid-cols-2 justify-center items-center bg-green-200 h-96 w-[700px] rounded-2xl shadow-green-900 shadow-2xl">
+                <Image src='/Premium Photo _ Vintage cinema videocamera.jpg' alt='fondo ' width={380} height={100} className='col-span-1 w-full h-full rounded-l-2xl'/>
+                <div className='col-span-1 flex flex-col gap-9 justify-center items-center'>
+                    <h1 className="text-3xl font-extrabold">login</h1>
+                    <input type="email" name="" id="" placeholder='email' className="p-2 rounded-md shadow-gray-950 shadow-inner focus:outline-none focus:outline-emerald-700 focus:shadow-none"/>
+                    <input type="password" name="" id=""  placeholder='password' className="p-2 rounded-md shadow-gray-950 shadow-inner focus:outline-none focus:outline-emerald-700 focus:shadow-none"/>
+                </div>
+            </main>
         </div>
     )
 }
