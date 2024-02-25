70d0878118ad208081a7efbef5e55bead222a7b3
eliyya <tecno_eli@hotmail.com>
2024-02-24 21:36:32 -0600

    add prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,32 @@
 import { MovieCard } from './MovieCard'
 import Nav from './Nav'
-import db from '@/lib/db'
-import { notFound } from 'next/navigation'
+import { prisma } from '@/lib/db'
 
 export default async function Home() {
 
-    const moviesQuery = await db
-        .rpc('get_movie_listings')
-
-    if (moviesQuery.error) {
-        console.error(moviesQuery.error)
-        return notFound()
-    }
-
-    const movies = moviesQuery.data.filter((m, i) => i === moviesQuery.data.findIndex((movie) => movie.id === m.id))
-
+    const movies = await prisma
+        .movies
+        .findMany({
+            where: {
+                movie_formats: {
+                    some: {
+                        functions: {
+                            some: {
+                                start_at: {
+                                    gte: new Date()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
     return (
         <div className="h-screen w-screen overflow-x-hidden">
-            <Nav/>
+            <Nav />
             <main className="grid grid-cols-4 grid-rows gap-8">
-                {movies.map(({name, id, image})=> (
-                    <MovieCard id={id} name={name} image={image} key={id}/>
+                {movies.map(({ name, id, image }) => (
+                    <MovieCard id={id} name={name} image={image} key={id} />
                 ))}
             </main>
         </div>
