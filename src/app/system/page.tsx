f9a7813efe803e7c5b54ca78928d999cba1b54fd
mael098 <juanvillegasmorales@outlook.com>
2024-07-09 12:15:16 -0600

    bug areglado

--- src/app/system/page.tsx
+++ src/app/system/page.tsx
@@ -1,6 +1,6 @@
 import { getSessionUser } from '@/lib/auth'
 import { COOKIE, ROLE } from '@/lib/constants'
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 import { AdminDashboard } from './AdminDashboard'
 import { PromoterDashboard } from './PromoterDashboard'
 import { SellerDashboard } from './SellerDashboard'
@@ -8,8 +8,8 @@ import { cookies } from 'next/headers'
 
 export default async function Dashboard() {
     const user = await getSessionUser()
-    if (!user) return notFound()
-    const like = parseInt(cookies().get(COOKIE.ADMIN_LIKE)?.value??'')
+    if (!user) redirect('/system/login')
+    const like = parseInt(cookies().get(COOKIE.ADMIN_LIKE)?.value ?? '')
     if (like == ROLE.ADMIN) return <AdminDashboard />
     if (like == ROLE.PROMOTER) return <PromoterDashboard />
     if (like == ROLE.TIKETMAN) return <SellerDashboard />
