a32b6624762413549b49d9fd3a4e5328b3f1623a
eliyya <tecno_eli@hotmail.com>
2024-07-19 06:47:39 -0600

    fix 404 system

--- src/app/system/page.tsx
+++ src/app/system/page.tsx
@@ -1,17 +1,23 @@
 import { getSessionUser } from '@/lib/auth'
-import { COOKIE, ROLE } from '@/lib/constants'
+import { COOKIE } from '@/lib/constants'
 import { notFound, redirect } from 'next/navigation'
 import { AdminDashboard } from './AdminDashboard'
 import { PromoterDashboard } from './PromoterDashboard'
 import { SellerDashboard } from './SellerDashboard'
 import { cookies } from 'next/headers'
+import { getLikeRole } from '@/lib/roles'
+import { Role } from '@prisma/client'
 
 export default async function Dashboard() {
     const user = await getSessionUser()
+
     if (!user) redirect('/system/login')
-    const like = parseInt(cookies().get(COOKIE.ADMIN_LIKE)?.value ?? '')
-    if (like == ROLE.ADMIN) return <AdminDashboard />
-    if (like == ROLE.PROMOTER) return <PromoterDashboard />
-    if (like == ROLE.TIKETMAN) return <SellerDashboard />
+    const like = getLikeRole(user.role, cookies().get(COOKIE.ADMIN_LIKE)?.value ?? '')
+
+
+    if (like == Role.MASTER) return <AdminDashboard />
+    if (like == Role.ADMIN) return <AdminDashboard />
+    if (like == Role.PROMOTER) return <PromoterDashboard />
+    if (like == Role.TIKETMAN) return <SellerDashboard />
     return notFound()
 }
